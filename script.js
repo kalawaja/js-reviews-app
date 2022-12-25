82a8b88b522ec8d5a6805b2608c65e4f0197dbf2
Ramazan İLTER <remzilter@gmail.com>
2022-12-26 00:03:52 +0300

    fixed and update script.js

--- script.js
+++ script.js
@@ -2,39 +2,39 @@
 const reviews = [
     {
       id: 1,
-      name: "susan smith",
+      name: "leila nunez",
       job: "web developer",
       img:
         "./images/friend-2.png",
       text:
-        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+        "Voluptatum eaque assumenda consequatur nihil repudiandae dolores repellat quam eos, est, unde ex labore magnam, aliquam laboriosam ad recusandae? Unde animi ducimus accusamus facere, ipsum corporis id vel aperiam laborum.",
     },
     {
       id: 2,
-      name: "anna johnson",
+      name: "serhat kilic",
       job: "web designer",
       img:
       "./images/friend-3.png",
       text:
-        "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+        "Quod provident commodi alias excepturi odit fugit accusamus consequatur? Mollitia, cum nam reiciendis molestiae atque aliquid beatae, necessitatibus tenetur, quas et sequi minima corporis fugiat. Cupiditate voluptate itaque dolorem debitis!",
     },
     {
       id: 3,
-      name: "peter jones",
+      name: "meiram sole",
       job: "intern",
       img:
       "./images/friend-4.png",
       text:
-        "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+        "Sapiente consectetur reiciendis obcaecati nulla eveniet ipsam laboriosam dignissimos laborum aperiam nemo ab error voluptates, dolore neque! Earum dolorum a voluptate sed ipsam eligendi optio quos velit explicabo! Provident, distinctio.",
     },
     {
       id: 4,
-      name: "bill anderson",
+      name: "adam relit",
       job: "the boss",
       img:
       "./images/friend-5.png",
       text:
-        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+        "Animi veritatis, facere nam nulla nihil dignissimos harum dolor, debitis itaque corporis quae voluptatibus ipsam? Pariatur ullam incidunt ea perspiciatis, hic, nulla quos nobis, iste dolorum expedita vitae dolores molestiae.",
     },
 ];
 // select items
@@ -52,5 +52,38 @@ let currentItem = 0;
 
 //load initial item
 window.addEventListener("DOMContentLoaded", function () {
-    
-})
+    showPerson();
+});
+
+// show person based on item
+function showPerson() {
+    const item = reviews[currentItem];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+}
+
+// show next person
+nextBtn.addEventListener("click", function() {
+    currentItem++;
+    if(currentItem > reviews.length - 1) {
+        currentItem = 0;
+    }
+    showPerson();
+});
+
+// show prev person
+prevBtn.addEventListener("click", function() {
+    currentItem--;
+    if(currentItem < 0) {
+        currentItem = reviews.length - 1;
+    }
+    showPerson();
+});
+
+// show random person
+randomBtn.addEventListener("click", function() {
+    currentItem = Math.floor(Math.random() * reviews.length);
+    showPerson();
+});
